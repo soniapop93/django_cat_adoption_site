bcccf566fa0776f2a8cc0437b720a32792100d52
soniapop93 <popsonia85@gmail.com>
2022-01-27 00:25:25 +0200

    started working on the html

--- adoption_cat_site/adoptions/urls.py
+++ adoption_cat_site/adoptions/urls.py
@@ -6,4 +6,5 @@ from django.urls import path
 app_name= 'adoptions'
 urlpatterns = [
     path('',views.index, name='index'),
+    path('<int:cat_id>/', views.detail, name='detail' ),
 ]
--- adoption_cat_site/adoptions/views.py
+++ adoption_cat_site/adoptions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Cat
+from django.shortcuts import render, get_object_or_404
+from .models import Cat, Cat_Details
+
 
 def index(request):
     latest_cat_list = Cat.objects.order_by('-pub_date')[:5]
@@ -7,3 +8,8 @@ def index(request):
         'latest_cat_list': latest_cat_list,
     }
     return render(request,'adoptions/index.html', context)
+
+def detail(request, cat_id):
+    cat = Cat.objects.get(pk=cat_id)
+    cat_details = Cat_Details.objects.get(details=cat.pk)
+    return render(request, 'adoptions/detail.html', {'cat':cat, 'cat_details':cat_details})
